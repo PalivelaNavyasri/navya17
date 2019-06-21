aea013d0dbb6c4f581645d73e17d6a0ae46b0ee4
unknown <abhinavyasurya777@gmail.com>
2019-06-21 15:19:36 +0530

    first commit

---
+++ src/main/java/com/example/navya17/Navya17Application.java
@@ -0,0 +1,13 @@
+package com.example.navya17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Navya17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Navya17Application.class, args);
+	}
+
+}
